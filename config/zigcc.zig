4c3aaaa145c5b7ad52a70d285e54f888e57e3fc5
Matheus Catarino <matheus-catarino@hotmail.com>
2024-06-20 11:36:14 -0300

    zigcc: fix rootPath

--- config/zigcc.zig
+++ config/zigcc.zig
@@ -5,7 +5,11 @@ pub fn buildZigCC(b: *std.Build) *std.Build.Step.Compile {
         .name = "zcc",
         .target = b.graph.host,
         .optimize = .ReleaseSafe,
-        .root_source_file = .{ .cwd_relative = b.fmt("{s}/tools/zigcc.zig", .{b.pathFromRoot(".")}) },
+        .root_source_file = .{ .cwd_relative = b.pathJoin(&.{ rootPath(), "..", "tools", "zigcc.zig" }) },
     });
     return exe;
+}
+
+fn rootPath() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
 }
